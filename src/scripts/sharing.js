cf9caf5a4cccf6c28c9fe8372d0eb2d1593c522f
unknown <amytang325@gmail.com>
2022-10-18 16:12:21 -0400

    code style fixes for sharing.js file

--- src/scripts/sharing.js
+++ src/scripts/sharing.js
@@ -10,7 +10,7 @@ function sendMessageToContentScript() {
     chrome.tabs.sendMessage(tabs[0].id, { greeting: 'hello' }, (response) => {
       console.log(response.farewell);
     });
-    chrome.tabs.sendMessage(tabs[0].id, {action: 'connect-to-socket-server'}, (response) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'connect-to-socket-server' }, (response) => {
       console.log(response.result);
     });
   });
